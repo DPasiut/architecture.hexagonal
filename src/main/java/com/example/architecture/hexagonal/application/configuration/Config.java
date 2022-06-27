27077c1938e0388b0df527ff5e3a65886bf11c62
DPasiut <domel1162@gmail.com>
2022-06-27 10:39:25 +0200

    inMemoryRepository implemented

--- src/main/java/com/example/architecture/hexagonal/application/configuration/Config.java
+++
@@ -1,2 +0,0 @@
-package com.example.architecture.hexagonal.application.configuration;class Config {
-}
---
+++ src/main/java/com/example/architecture/hexagonal/domain/port/InMemoryContentRepository.java
@@ -0,0 +1,2 @@
+package com.example.architecture.hexagonal.domain.port;class InMemoryContentRepository {
+}
