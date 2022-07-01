505093d4a3761ecb6f26440abe96700700705c5a
DPasiut <domel1162@gmail.com>
2022-07-01 14:09:20 +0200

    refactor

--- src/main/java/com/example/architecture/hexagonal/application/rest/PublishContentController.java
+++ src/main/java/com/example/architecture/hexagonal/application/rest/PublishContentController.java
@@ -6,13 +6,14 @@ import com.example.architecture.hexagonal.domain.handlers.unpublish.UnpublishCom
 import com.example.architecture.hexagonal.domain.handlers.unpublish.UnpublishCommandHandler;
 import com.example.architecture.hexagonal.domain.valueobjects.DmsId;
 import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@AllArgsConstructor
+@RequiredArgsConstructor
 @RestController
 @RequestMapping(path = "/content/{dmsId}")
 public class PublishContentController {
